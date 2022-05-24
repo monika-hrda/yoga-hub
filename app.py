ef59777270c48b9718e22097e4cfdecf01bf113c
monika-hrda <mon.hrda@gmail.com>
2022-05-24 20:52:10 +0000

    Add Register template and styling

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -21,6 +22,11 @@ mongo = PyMongo(app)
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
