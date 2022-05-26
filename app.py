77cd08bdf2dad46cfadaf79c6e33a75fb4a5cdba
monika-hrda <mon.hrda@gmail.com>
2022-05-26 23:58:08 +0000

    Add custom 404 & 500 error pages

--- app.py
+++ app.py
@@ -201,7 +201,19 @@ def search():
     return render_template("classes.html", classes=classes)
 
 
+# 404 error (page not found)
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html", error=error), 404
+
+
+# 500 error (internal server error)
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500.html", error=error), 500
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
